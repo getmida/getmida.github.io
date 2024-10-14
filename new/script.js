9647e56eb255fa0e8f89905a15cb45a5f585efaf
REBECCA ADENSON <radenson@renmoney.com>
2024-10-14 18:25:23 +0100

    add website-rebuild

---
+++ new/script.js
@@ -0,0 +1,49 @@
+//accordion-cycle
+
+const accordionItems = document.querySelectorAll('.accordion-item')
+const desktopAccordionImages = document.querySelectorAll(
+  '.accordion-image-container .accordion-image'
+)
+const mobileAccordionImages = document.querySelectorAll(
+  '.accordion-image-container-mobile .accordion-image'
+)
+
+let currentIndexs = 0
+const intervalTime = 3000 // Time between each accordion auto-click (in milliseconds)
+
+// Function to handle accordion item click
+function handleAccordionItemClick(index) {
+  accordionItems.forEach((item, i) => {
+    const isActive = i === index
+    item.classList.toggle('active', isActive)
+    const accordionItemBody = item.querySelector('.accordion-item-body')
+    accordionItemBody.style.maxHeight = isActive
+      ? accordionItemBody.scrollHeight + 'px'
+      : 0
+  })
+
+  // Show the associated image and hide others for desktop
+  desktopAccordionImages.forEach((image, i) => {
+    image.style.display = i === index ? 'block' : 'none'
+  })
+
+  // Show the associated image and hide others for mobile
+  mobileAccordionImages.forEach((image, i) => {
+    image.style.display = i === index ? 'block' : 'none'
+  })
+}
+
+// Function to auto-cycle through accordion items
+function autoCycleAccordion() {
+  handleAccordionItemClick(currentIndexs)
+
+  // Move to the next item, or loop back to the first item
+  currentIndexs = (currentIndexs + 1) % accordionItems.length
+}
+
+// Set up automatic cycling of accordion items
+setInterval(autoCycleAccordion, intervalTime)
+
+// Activate the first accordion item and show its image by default
+handleAccordionItemClick(0)
+
